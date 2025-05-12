6794f04813e6102a87faf643203dc97bde05dc1a
GeorgHI <hansssnah666@gmail.com>
2025-05-12 15:02:50 +0200

    improved mobile looks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Footer: React.FC = () => {
     return (
 
-        <footer className="row-start-3 striped-navbar flex gap-6 pt-1 flex-wrap items-center justify-between h-24">
+        <footer className="row-start-3 striped-navbar flex flex-col sm:flex-row flex-wrap items-center justify-between gap-4 p-4 sm:h-24">
             <a className="items-center flex p-3">
                 <Image
                     aria-hidden
@@ -17,6 +17,7 @@ const Footer: React.FC = () => {
             <a className="flex items-center gap-2">
                 <p className="text-white">
                     Helsingkrona Nation, Tornavägen 3C, 223 64 Lund
+                    <br></br>
                     Tel: 046-14 60 64, Mail: kuratel@helsingkrona.se
                 </p>
             </a>
