12cb5183da7d18cd9f66c8feedefcab7a801548e
GeorgHI <hansssnah666@gmail.com>
2025-07-15 22:48:22 +0200

    implemeted thank you page

---
+++ app/tack/page.tsx
@@ -0,0 +1,14 @@
+export default function TackPage() {
+
+
+return (
+      <div className="p-2">
+        <div className="text-center p-6 bg-darkerGreen max-w-2xl shadow-lg rounded-lg mx-auto">
+          <p className="text-lg font-medium">
+            Tack för din anmälan! <br></br>Du kommer att få ett mail som bekräftar din anmälan inom kort.<br></br> Notera att anmälan inte medför platsgaranti.
+          </p>
+        </div>
+      </div>
+    );
+
+}
